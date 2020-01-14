4551f11dfba54860fec5e898d9b5ce2ad75fe6d8
annaaisyaNYP <190805H@mymail.nyp.edu.sg>
2020-01-14 11:03:22 +0800

    flask practical 3 started

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from forms import CreateUserForm
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/createUser', methods=['GET', 'POST'])
+def createUser():
+ createUserForm = CreateUserForm(request.form)
+ if request.method == 'POST' and createUserForm.validate():
+    return redirect(url_for('home'))
+ return render_template('createUser.html', form=createUserForm)
 
 if __name__ == '__main__' :
     app.run()
